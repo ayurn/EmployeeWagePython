d52470bfd3c315a87474f2f1c104ce453d0ce31a
ayurn <ninaweayur@gmail.com>
2021-06-27 01:04:07 +0530

    [Ayur] Added : checking if employee is present or not.

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,1 +1,12 @@
+import random
+
 print("Welcome to Employee Wage Computation")
+
+IS_ABSENT = 0
+IS_PRESENT = 1
+
+attendance_check = random.randint(0, 1)
+if attendance_check == IS_PRESENT:
+    print("Employee is present")
+elif attendance_check == IS_ABSENT:
+    print("Employee is absent")
