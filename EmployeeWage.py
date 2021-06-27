39418fd9ff7f93e81f0522d9a46b1f3a9fa642ff
ayurn <ninaweayur@gmail.com>
2021-06-27 18:35:19 +0530

    [Ayur] Added : Feature to find employee wage for a month

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,8 +1,8 @@
 """
 @Author: Ayur Ninawe
-@Date: 2021-06-27 17:12:30
+@Date: 2021-06-27 17:30:30
 @Last Modified by: Ayur Ninawe
-@Last Modified time: 2021-06-27 17:12:30
+@Last Modified time: 2021-06-27 17:30:30
 @Title : Employee Wage Computation.
 
 """
@@ -14,7 +14,9 @@ print("Welcome to Employee Wage Computation")
 WAGE_PER_HOUR = 20
 FULL_TIME_HOURS = 8
 PART_TIME_HOURS = 4
+DAYS_FOR_MONTH = 20
 day_hour = 0
+
 
 def is_absent():
     return day_hour
@@ -27,9 +29,11 @@ def is_full_time():
 def is_part_time():
     return PART_TIME_HOURS
 
+
 employee_status = {0: is_absent, 1: is_full_time, 2: is_part_time}
 attendance_check = random.randint(0, 2)
 day_hour = employee_status.get(attendance_check)()
 
-employee_wage = WAGE_PER_HOUR * day_hour
+daily_employee_wage = WAGE_PER_HOUR * day_hour
+employee_wage = daily_employee_wage * DAYS_FOR_MONTH
 print("employee wage is : ", employee_wage)
