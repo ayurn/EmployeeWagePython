a01b876faaf0009f49cd193791685cb8d9421b51
ayurn <ninaweayur@gmail.com>
2021-06-27 16:57:41 +0530

    [Ayur] Added : Calculating Emplayee daily wage.

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,12 +1,27 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-06-27 16:56:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-06-27 16:56:30
+@Title : Employee Wage Computation.
+
+"""
+
 import random
 
 print("Welcome to Employee Wage Computation")
 
 IS_ABSENT = 0
 IS_PRESENT = 1
+WAGE_PER_HOUR = 20
 
+full_day_hour = 0
 attendance_check = random.randint(0, 1)
 if attendance_check == IS_PRESENT:
-    print("Employee is present")
+    full_day_hour = 8
+
 elif attendance_check == IS_ABSENT:
-    print("Employee is absent")
+    pass
+
+employee_wage = WAGE_PER_HOUR * full_day_hour
+print("Employee wage is : ", employee_wage)
